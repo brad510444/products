199780a25eff967f499a3b337366a4ccba586fb3
Brad Hou <brad510444@gmail.com>
2020-07-21 23:01:15 +0800

    add column

--- products.py
+++ products.py
@@ -10,3 +10,8 @@ print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,名稱\n')#寫入檔案會形成亂碼，於是要再寫入時跟他說要用utf-8這個編碼
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')#加法只能字串跟字串
